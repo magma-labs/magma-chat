dbc4c79f08c29849a0f6e54595c9618c9f5bbaca
Obie Fernandez <obiefernandez@gmail.com>
2023-04-14 15:50:25 -0600

    make sure its not an ordering issue with importmaps

--- config/importmap.rb
+++ config/importmap.rb
@@ -4,11 +4,11 @@ pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
-pin_all_from "app/javascript/controllers", under: "controllers"
-pin_all_from "app/javascript/channels", under: "channels"
-pin_all_from "app/javascript/config", under: "config"
 pin "@rails/actioncable", to: "actioncable.esm.js", preload: true
 pin "morphdom", to: "https://ga.jspm.io/npm:morphdom@2.6.1/dist/morphdom.js", preload: true
 pin "lodash.debounce", to: "https://ga.jspm.io/npm:lodash.debounce@4.0.8/index.js"
 pin "cable_ready", to: 'cable_ready.js', preload: true
 pin "stimulus_reflex", to: "stimulus_reflex.js", preload: true
+pin_all_from "app/javascript/controllers", under: "controllers"
+pin_all_from "app/javascript/channels", under: "channels"
+pin_all_from "app/javascript/config", under: "config"
