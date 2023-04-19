b14cf360b995fe434d6acce29b28dd16216c49b3
Aldope <aldompe95@gmail.com>
2023-04-19 13:35:48 -0600

    Redirect to chats page if user is already logged

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,12 @@
 class HomeController < ApplicationController
+  before_action :redirect_if_logged_in, only: [:index]
+
   def index
+  end
+
+  private
+
+  def redirect_if_logged_in
+    redirect_to chats_path if current_user
   end
 end
