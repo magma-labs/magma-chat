3a982fa39361855b1c1b4ad4923432ef5797db6a
Carlos Araoz <c.araoz.g@gmail.com>
2023-04-24 09:22:21 -0600

    Remove ChatSearch.tensor spec

--- spec/models/chat_search_spec.rb
+++ spec/models/chat_search_spec.rb
@@ -42,61 +42,6 @@ RSpec.describe ChatSearch do
     end
   end
 
-  describe '.tensor' do
-    subject(:instance) { described_class.tensor(query) }
-
-    let(:user) { create(:user) }
-    let(:query) { message_content.split(' ').first }
-    let(:chat) { create(:chat, message_count: 1, user: user) }
-    let(:message) { chat.messages.last }
-    let(:message_content) { message.content }
-    let(:marqo_client) { double('Marqo') }
-    let(:marqo_response) do
-      {
-        hits: [hit],
-        query: query
-      }
-    end
-    let(:hit) do
-      {
-        '_id': Faker::Number.number(digits: 10),
-        chat_id: chat.id,
-        content: message_content,
-        role: message.role
-      }
-    end
-
-    before do
-      allow(Gpt).to receive(:chat)
-      allow(Marqo).to receive(:client).and_return(marqo_client)
-
-      allow_any_instance_of(Chat).to receive(:add_context_messages)
-
-      chat
-    end
-
-    it 'returns an instance with results' do
-      expect(marqo_client).to receive(:search).and_return(marqo_response)
-
-      expect(message_content).to include query
-      expect(instance.query).to eq query
-      expect(instance.results.count).to eq 1
-
-      first_result = instance.results.first
-
-      expect(first_result).to be_a OpenStruct
-      expect(first_result.chat).to eq chat
-      expect(first_result.to_partial_path).to eq 'chats/result'
-
-      first_message = first_result.messages.first
-      expect(first_result.messages.count).to eq 1
-      expect(first_message).to be_a OpenStruct
-      expect(first_message.id).to eq hit[:_id]
-      expect(first_message.content).to eq hit[:content]
-      expect(first_message.role).to eq hit[:role]
-    end
-  end
-
   describe '.tag' do
     subject(:instance) { described_class.tag(query) }
 
