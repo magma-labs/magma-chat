225e12c945f600118042a777420343e0e6a77067
Carlos Araoz <c.araoz.g@gmail.com>
2023-04-28 11:19:30 -0600

    CircleCI - build tailwindcss and save test results

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,13 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
+require 'simplecov'
+
+SimpleCov.start 'rails' do
+  add_group 'Services', 'app/services'
+  add_group 'Reflexes', 'app/reflexes'
+  add_group 'Constraints', 'app/constraints'
+end
+
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 # Prevent database truncation if the environment is production
