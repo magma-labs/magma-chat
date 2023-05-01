35a69e6ae7c1143c97a95c22238ea8a0fcc84cf4
Obie Fernandez <obiefernandez@gmail.com>
2023-05-01 11:51:24 -0600

    make settings more bulletproof

--- app/models/concerns/settings.rb
+++ app/models/concerns/settings.rb
@@ -2,7 +2,19 @@ module Settings
   include ActiveSupport::Concern
 
   def settings
-    RecursiveOpenStruct.new(super, recurse_over_arrays: true)
+    @settings ||= RecursiveOpenStruct.new(begin
+      super.then do |hash|
+        if hash.kind_of? String
+          # TODO: why is this workaround needed sometimes?
+          JSON.parse(hash)
+        else
+          hash
+        end
+      end
+    rescue => exception
+      Rails.logger.error("Error loading settings for #{self.class.name} #{self.id}: #{exception.message}")
+      {}
+    end, recurse_over_arrays: true)
   end
 
   def raw_settings
